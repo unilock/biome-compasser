5b8f920ca0bc10103c0b3e368362a50d92b28c5c
unilock <unilock@fennet.rentals>
2023-08-31 21:06:30 -0400

    Remove client-side init
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/dev/mayaqq/biomecompass/client/BiomeCompassClient.java
+++
@@ -1,9 +0,0 @@
-package dev.mayaqq.biomecompass.client;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class BiomeCompassClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}
