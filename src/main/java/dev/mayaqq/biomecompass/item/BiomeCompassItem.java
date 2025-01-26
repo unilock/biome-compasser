7e1840c8d4361d332876b23da4c5de98dd355a43
unilock <unilock@fennet.rentals>
2025-01-26 10:14:27 -0500

    Re-implement Polymer support and fix teleport height
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/dev/mayaqq/biomecompass/item/BiomeCompassItem.java
+++ src/main/java/dev/mayaqq/biomecompass/item/BiomeCompassItem.java
@@ -4,10 +4,14 @@ import dev.mayaqq.biomecompass.BiomeCompass;
 import dev.mayaqq.biomecompass.gui.BiomeSelectionGui;
 import dev.mayaqq.biomecompass.helper.TextHelper;
 import dev.mayaqq.biomecompass.registry.BiomeCompassItems;
+import eu.pb4.polymer.core.api.item.PolymerItem;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.CompassItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtHelper;
 import net.minecraft.nbt.NbtOps;
@@ -20,13 +24,14 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.GlobalPos;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Optional;
 
-public class BiomeCompassItem extends Item {
+public class BiomeCompassItem extends Item implements PolymerItem {
     public static final String BIOME_NAME_KEY = BiomeCompass.id("biome_name").toString();
     public static final String BIOME_DIMENSION_KEY = BiomeCompass.id("biome_dimension").toString();
     public static final String BIOME_POS_KEY = BiomeCompass.id("biome_pos").toString();
@@ -72,9 +77,8 @@ public class BiomeCompassItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if (user instanceof ServerPlayerEntity player) {
             if (player.isCreative() && player.isSneaking() && player.getStackInHand(hand).hasNbt() && player.getStackInHand(hand).getNbt().contains(BIOME_POS_KEY)) {
-                BlockPos pos = NbtHelper.toBlockPos(player.getStackInHand(hand).getNbt().getCompound(BIOME_POS_KEY));
-                // TODO: somehow get top block at position? (y=120 is usually safe)
-                player.requestTeleport(pos.getX(), 120, pos.getZ());
+                BlockPos pos = world.getTopPosition(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, NbtHelper.toBlockPos(player.getStackInHand(hand).getNbt().getCompound(BIOME_POS_KEY)));
+                player.requestTeleport(pos.getX(), pos.getY(), pos.getZ());
                 return TypedActionResult.success(user.getStackInHand(hand));
             }
 
@@ -104,5 +108,26 @@ public class BiomeCompassItem extends Item {
 
     private static Optional<RegistryKey<World>> getBiomeDimension(NbtCompound nbt) {
         return World.CODEC.parse(NbtOps.INSTANCE, nbt.get(BIOME_DIMENSION_KEY)).resultOrPartial(BiomeCompass.LOGGER::error);
+    }
+
+    @Override
+    public Item getPolymerItem(ItemStack itemStack, @Nullable ServerPlayerEntity serverPlayerEntity) {
+        return Items.COMPASS;
+    }
+
+    @Override
+    public ItemStack getPolymerItemStack(ItemStack itemStack, TooltipContext context, @Nullable ServerPlayerEntity player) {
+        ItemStack fake = PolymerItem.super.getPolymerItemStack(itemStack, context, player);
+        if (hasBiome(itemStack)) {
+            NbtCompound nbt = itemStack.getNbt().copy();
+            nbt.put(CompassItem.LODESTONE_POS_KEY, nbt.getCompound(BIOME_POS_KEY));
+            nbt.put(CompassItem.LODESTONE_DIMENSION_KEY, nbt.getCompound(BIOME_DIMENSION_KEY));
+            nbt.putBoolean(CompassItem.LODESTONE_TRACKED_KEY, nbt.getBoolean(BIOME_TRACKED_KEY));
+            fake.setNbt(nbt);
+//          if (!PolymerResourcePackUtils.hasPack(player)) {
+            fake.addEnchantment(Enchantments.INFINITY, 0);
+//          }
+        }
+        return fake;
     }
 }
